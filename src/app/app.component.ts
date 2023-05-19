5c34cd79787b5bcd5f4639d543f45222c771a93c
Mike Costa <cbyk.mocosta@voegol.com.br>
2023-05-19 14:45:17 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+//import * as utilsBr from 'js-brasil';
+import { utilsBr } from 'js-brasil';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-v15-libs-brasil';
+
+  constructor() {
+    console.log(utilsBr.ESTADOS, 'js-brasil - utilsBr.ESTADOS');
+    console.log(utilsBr.MASKS.cpf, 'js-brasil - utilsBr.MASKS.cpf');
+  }
 }
